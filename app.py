b220441f3a8795b2e53ffaec9a5cf69950887fcd
IKEENOO <kudryavtsev.dev@gmail.com>
2024-05-12 17:26:21 +0300

    Merge branch 'build'

--- app.py
+++ app.py
@@ -44,24 +44,23 @@ def spiral():
 
 if __name__ == '__main__':
   root = Tk()
+
   root.title('Библиотека 3D резьб')
-  root.iconbitmap(default="favicon.ico")
+  root.iconbitmap(default = "favicon.ico")
+
   end_conditions = ["Задать длину", "На всю длину"]
-  combobox = ttk.Combobox(values=end_conditions)
-  combobox.current(0)
-  combobox.pack(anchor=NW, padx=6, pady=6)
-  entry = ttk.Entry(root, width=23)
-  entry.pack(anchor=NW, padx=6, pady=6)
-  entry.insert(0, "10.0 мм")
-  
-  clicks = 0
-  
-  def click_button():
-    label["text"] = entry.get()   
+  cbox_end_conditions = ttk.Combobox(values = end_conditions)
+  cbox_end_conditions.current(0)
+  cbox_end_conditions.pack(anchor = NW, padx = 6, pady = 6)
+
+  entry_end_conditions = ttk.Entry(root, width = 23)
+  entry_end_conditions.pack(anchor = NW, padx = 6, pady = 6)
+  entry_end_conditions.insert(0, "10.0 мм")
 
   
-  btn = ttk.Button(text="Click Me", command=click_button)
-  btn.pack(anchor=NW, padx=6, pady=6)
+  def click_button():
+    label["text"] = entry_end_conditions.get()   
+
   label = ttk.Label()
   label.pack(anchor=NW, padx=6, pady=6)
 
